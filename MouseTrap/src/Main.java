f43bc4bdab80d0f30fa237220631ee14f59db00d
Whiko <julie.guignabert@gmail.com>
2014-03-13 17:29:19 +0100

    Création de la classe  Main et mise à jour de Map

---
+++ MouseTrap/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		Map carte = new Map();
+		carte.afficheMap("map/Map_lvl1.txt");
+	}
+
+}
--- MouseTrap/src/Map.java
+++ MouseTrap/src/Map.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Map 
 {
@@ -9,56 +8,25 @@ public class Map
 	{
 		int x = 20;
 		int y = 20;
+		int caractere = 0;
 		carte = new int [20][20];	//taille de la map
 		
-		InputStream fichier = new FileInputStream(path); 
-		InputStreamReader ipsr=new InputStreamReader(fichier);
-		BufferedReader br=new BufferedReader(ipsr);
-		String ligne;
-		
-		/*while ((ligne=br.readLine())!=null)		//ça va pas.
-		{	
-			for(int i=0; i<y; i++)
-			{
-				for(int j=0; j<x; j++)
-				{
-					carte[i][j] = ligne.charAt(j);
-					if(carte[i][j]==1)
-					{
-						
-					}
-				}
-			}
-		}	*/
-		
-		int i=0;
-		int j=0;
-		Scanner sc = null;
-        try {
-		    sc = new Scanner(fichier);
-		    while (sc.hasNextLine()) 
-		    {
-		        for (char c : sc.next().toCharArray()) 
-		        {
-		            carte[i][j] = (int)c;
-		            
-		            if(carte[i][j]==1)
-		            {
-		            	
-		            }
-		            
-		            i++;
-		        	//affiche les blocs
-		        }
-		        
-		        j++;
-		    }
-		} 
-		finally 
+		FileInputStream fichier = new FileInputStream(path); 
+		caractere = fichier.read();
+		for(int i=0; i<x; i++)
 		{
-		    if (sc != null)
-		        sc.close();
+			for(int j=0; j<y; j++)
+			{
+				if(caractere != 10) //'10' = retour a la ligne
+				{
+					carte[i][j] = caractere;
+					System.out.println(carte[i][j]+" ");
+				}
+				
+				else
+					System.out.println("\n");
+			}		
 		}
-		
+		fichier.close();
 	}
 }
